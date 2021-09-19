faafe12983e390e12751544f82e6c1acd1ebefee
irongut <murray.dave@outlook.com>
2021-09-19 22:36:45 +0100

    initial project import

---
+++ src/EditRelease/Options.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+using System.Collections.Generic;
+
+namespace EditRelease
+{
+    public class Options
+    {
+#if DEBUG
+        [Option(shortName: 't', longName: "token", Required = false, HelpText = "Authentication token, use either GITHUB_TOKEN or a Personal Access Token.")]
+
+#else
+        [Option(shortName: 't', longName: "token", Required = true, HelpText = "Authentication token, use either GITHUB_TOKEN or a Personal Access Token.")]
+
+#endif
+        public string Token { get; set; }
+
+        [Option(shortName: 'i', longName: "id", Required = true, HelpText = "The id of the release, e.g. github.event.release.id.")]
+        public int ReleaseId { get; set; }
+
+        [Option(shortName: 'n', longName: "name", Required = false, HelpText = "The name of the release.")]
+        public string Name { get; set; }
+
+        [Option(shortName: 'm', longName: "namemode", Required = false, HelpText = "Whether to add or replace the release name.")]
+        public string NameMode { get; set; }
+
+        [Option(shortName: 'd', longName: "draft", Required = false, HelpText = "Set true makes the release a draft, false publishes the release.")]
+        public bool Draft { get; set; }
+
+        [Option(shortName: 'p', longName: "prerelease", Required = false, HelpText = "Set true to identify the release as a pre-release, false to identify the release as a full release.")]
+        public bool Prerelease { get; set; }
+
+        [Option(shortName: 'b', longName: "body", Required = false, HelpText = "Text for the body of the release.")]
+        public string Body { get; set; }
+
+        [Option(shortName: 'o', longName: "bodymode", Required = false, HelpText = "Whether to add or replace the release body.")]
+        public string BodyMode { get; set; }
+
+        [Option(shortName: 'f', longName: "files", Separator = ',', Required = false, HelpText = "Comma separated list of files whose content will be added after Body.")]
+        public IEnumerable<string> Files { get; set; }
+
+        [Option(shortName: 's', longName: "spacing", Required = false, HelpText = "Number of blank lines between each file in the release body.", Default = 1)]
+        public int Spacing { get; set; }
+    }
+}
---
+++ src/EditRelease/Program.cs
@@ -0,0 +1,84 @@
+﻿using CommandLine;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace EditRelease
+{
+    internal static class Program
+    {
+        private static async Task<int> Main(string[] args)
+        {
+            return await Parser.Default.ParseArguments<Options>(args)
+                                       .MapResult(o => RunActionAsync(o),
+                                                  _ => Task.FromResult(-1)) // invalid arguments
+                                       .ConfigureAwait(false);
+        }
+
+        private static async Task<int> RunActionAsync(Options options)
+        {
+            string repo = Environment.GetEnvironmentVariable("GITHUB_REPOSITORY");
+            if (string.IsNullOrWhiteSpace(repo) || !repo.Contains("/"))
+            {
+                Console.WriteLine("Error: GITHUB_REPOSITORY environment variable not found.");
+                return -2;
+            }
+
+#if DEBUG
+            options.Token = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+#endif
+            if (string.IsNullOrWhiteSpace(options.Token))
+            {
+                Console.WriteLine("Error: Authentication token not found, use either GITHUB_TOKEN or a Personal Access Token.");
+                return -2;
+            }
+
+            Console.WriteLine($"{Assembly.GetExecutingAssembly().GetName().Name} v{Assembly.GetExecutingAssembly().GetName().Version} started...");
+            Console.WriteLine($"Repository: {repo}, Release Id: {options.ReleaseId}");
+
+            try
+            {
+                string owner = repo.Split("/")[0];
+                string repoName = repo.Split("/")[1];
+
+                GitHubClient client = new(new ProductHeaderValue("taranis-edit-release-action"))
+                {
+                    Credentials = new Credentials(options.Token)
+                };
+
+                Release release = await client.Repository.Release.Get(owner, repoName, options.ReleaseId).ConfigureAwait(false);
+
+                if (release == null)
+                {
+                    Console.WriteLine($"Error: Unable to find Release Id {options.ReleaseId} in {repo}.");
+                    return -2;
+                }
+
+                Console.WriteLine($"Release Found - Id: {release.Id}, Tag: {release.TagName}, Author: {release.Author.Login}.");
+
+                ReleaseUpdate updateRelease = release.ToUpdate();
+                updateRelease.Body = $"{release.Body}{Environment.NewLine}{Environment.NewLine}Test";
+
+                var result = await client.Repository.Release.Edit(owner, repoName, options.ReleaseId, updateRelease).ConfigureAwait(false);
+                if (result != null)
+                {
+                    Console.WriteLine($"Release Updated - Id: {release.Id}, Tag: {release.TagName}, Author: {release.Author.Login}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Error: Unable to update Release Id: {release.Id}.");
+                    return -2;
+                }
+
+                return 0; // success
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.GetType()} - {ex.Message}");
+                return -3; // unhandled error
+            }
+        }
+    }
+}
