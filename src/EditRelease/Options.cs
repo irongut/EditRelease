61dca0ca387b87e7a0459364883333e4a000eeb8
irongut <murray.dave@outlook.com>
2021-09-21 21:42:42 +0100

    converted bool params to string to make action yaml easier

--- src/EditRelease/Options.cs
+++ src/EditRelease/Options.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 
 namespace EditRelease
@@ -20,20 +21,50 @@ namespace EditRelease
         [Option(shortName: 'n', longName: "name", Required = false, HelpText = "The name of the release.")]
         public string Name { get; set; }
 
-        [Option(shortName: 'm', longName: "replacename", Required = false, HelpText = "Set true to replace the release name, false to add to the release name.", Default = false)]
-        public bool ReplaceName { get; set; }
+        [Option(shortName: 'm', longName: "replacename", Required = false, HelpText = "Set true to replace the release name, false to add to the release name.")]
+        public string ReplaceNameString { get; set; }
+
+        public bool ReplaceName => !string.IsNullOrWhiteSpace(ReplaceNameString) && ReplaceNameString.Equals("true", StringComparison.OrdinalIgnoreCase);
 
         [Option(shortName: 'd', longName: "draft", Required = false, HelpText = "Set true makes the release a draft, false publishes the release.")]
-        public bool? Draft { get; set; }
+        public string DraftString { get; set; }
+
+        public bool? Draft
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(DraftString))
+                    return null;
+                else if (DraftString.Equals("true", StringComparison.OrdinalIgnoreCase))
+                    return true;
+                else
+                    return false;
+            }
+        }
 
         [Option(shortName: 'p', longName: "prerelease", Required = false, HelpText = "Set true to identify the release as a pre-release, false to identify the release as a full release.")]
-        public bool? Prerelease { get; set; }
+        public string PrereleaseString { get; set; }
+
+        public bool? Prerelease
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(PrereleaseString))
+                    return null;
+                else if (PrereleaseString.Equals("true", StringComparison.OrdinalIgnoreCase))
+                    return true;
+                else
+                    return false;
+            }
+        }
 
         [Option(shortName: 'b', longName: "body", Required = false, HelpText = "Text for the body of the release.")]
         public string Body { get; set; }
 
         [Option(shortName: 'o', longName: "replacebody", Required = false, HelpText = "Set true to replace the release body, false to add to the release body.", Default = false)]
-        public bool ReplaceBody { get; set; }
+        public string ReplaceBodyString { get; set; }
+
+        public bool ReplaceBody => !string.IsNullOrWhiteSpace(ReplaceBodyString) && ReplaceBodyString.Equals("true", System.StringComparison.OrdinalIgnoreCase);
 
         [Option(shortName: 'f', longName: "files", Separator = ',', Required = false, HelpText = "Comma separated list of files whose content will be added after Body.")]
         public IEnumerable<string> Files { get; set; }
